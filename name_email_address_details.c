9b2c2da4fa44d9af726226330dab361fef69563e
dhanush0909 <92786961+dhanush0909@users.noreply.github.com>
2022-08-17 13:41:57 +0530

    Add files via upload

---
+++ name_email_address_details.c
@@ -0,0 +1,7 @@
+//Assignment 1 : To print the email, mobile, occupation of a person on three different Lines
+
+#include <stdio.h>
+void main()
+{
+    printf("Email : dhanush0909@gmail.com\n Mobile: 7995880528\n Occupation : Student");
+}
