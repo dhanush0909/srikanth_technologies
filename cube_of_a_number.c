7f1a51151116765c5cc984f50737c3205236fd91
dhanush0909 <92786961+dhanush0909@users.noreply.github.com>
2022-08-17 13:42:34 +0530

    Add files via upload

---
+++ cube_of_a_number.c
@@ -0,0 +1,9 @@
+//Assignement 2 : To print the cube of a given number
+#include <stdio.h>
+void main(){
+int n, cube;
+printf("Enter the number to find the cube of it: ");
+scanf("%d",&n);
+cube=n*n*n;
+printf("The cube of %d is  %d",n,cube);
+}
