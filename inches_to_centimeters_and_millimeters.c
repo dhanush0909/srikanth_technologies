db6041f97504c3c4e3bee36ba12931c14b240aa6
dhanush0909 <92786961+dhanush0909@users.noreply.github.com>
2022-08-17 14:01:16 +0530

    Update inches_to_centimeters_and_millimeters.c

--- inches_to_centimeters_and_millimeters.c
+++ inches_to_centimeters_and_millimeters.c
@@ -1,1 +1,14 @@
 
+//Assignment 3 : To convert the given inches to centimeters and millimeters
+
+#include <stdio.h>
+void main()
+{
+int inches;
+float centimeters, millimeters;
+    printf("Enter the number of inches to be converted to centimeters and millileters : ");
+    scanf("%d",&inches);
+    centimeters=inches*2.54;
+    millimeters=inches*25.4;
+    printf("the values are %f and %f respectively",centimeters,millimeters);
+}
