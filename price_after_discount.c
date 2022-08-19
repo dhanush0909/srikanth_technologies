1bbaf5d50251c4f38ea56f5cce7ba47a6ac9d9a4
dhanush0909 <92786961+dhanush0909@users.noreply.github.com>
2022-08-19 16:21:23 +0530

    tax 12% Levied

--- price_after_discount.c
+++ price_after_discount.c
@@ -1,14 +1,20 @@
+// To print net amount for given price 
 #include<stdio.h>
 void main()
 {
-float price, quantity;
-float discount = 20.0/100.0;
-float discount_amount;
-printf("Enter the price and quantity : \n");
-scanf("%f %f",&price, &quantity);
-printf("Price after discount : ");
-discount_amount=price*discount;
-price-=discount_amount;
-price*=quantity;
-printf("%f",price);
+  int sm=1;
+  float price,net_price, quantity;
+  float discount = 20.0/100.0;
+  float discount_amount;
+  float tax_amount;
+  float tax = 12.0/100.0;
+  printf("Enter the price and quantity : \n");
+  scanf("%f %f",&price, &quantity);
+  price*=quantity;
+  discount_amount=price*discount;
+  price-=discount_amount;
+  tax_amount=price*tax;
+  net_price=tax_amount+price;
+  printf(" price         :     %7.2f\n discount      :     %7.2f\n tax           :     %7.2f\n ------------------------------\n net price     :     %7.2f\n", price+discount_amount,discount_amount, tax_amount, net_price);
+  printf("Please visit again %c",sm);
 }
